15b98f06a0d0165cecb9595ffab1b1deba7b8932
mounika444 <41731937+mounika444@users.noreply.github.com>
2018-07-26 21:07:55 -0700

    Create vowel.java

---
+++ vowel.java
@@ -0,0 +1,20 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		char ch='z';
+		if(ch == 'z')
+		System.out.println(ch+ "is constant");
+		else
+		System.out.println(ch+ "is vowel");
+
+		
+	}
+}
