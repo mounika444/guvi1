bdf1e8fb5010236a8c30dcdb1431713150017dd7
mounika444 <41731937+mounika444@users.noreply.github.com>
2018-07-26 21:48:02 -0700

    Rename leap.java to leap1.java

---
+++ leap1.java
@@ -0,0 +1,19 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		double i=2015;
+		
+		if(i==2015)
+System.out.println("no");
+else
+System.out.println("yes");
+	}
+}
