0171e316e7b42a21e407a1f00ece117bbbbed106
mounika444 <41731937+mounika444@users.noreply.github.com>
2018-07-26 21:15:34 -0700

    Create alphabet.java

---
+++ alphabet.java
@@ -0,0 +1,22 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		char ch='z';
+		if(ch == 'z')
+		System.out.println(ch+ "is alphabet");
+		else
+		System.out.println(ch+ "is not a alphabet");
+
+		
+		
+
+	}
+}
