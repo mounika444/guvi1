ab94029fc0f3b2d9eaa30c2205d0a0f9e0e9e31f
mounika444 <41731937+mounika444@users.noreply.github.com>
2018-07-26 21:25:44 -0700

    Create three.java

---
+++ three.java
@@ -0,0 +1,32 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int x=sc.nextInt();
+		int y=sc.nextInt();
+		int z=sc.nextInt();
+		System.out.println("enter a three integer");
+		if( x > y&& x > z )
+		System.out.println("x");
+		else
+		if(y > x&& y > z)
+		System.out.println("y");
+		else
+		if(z > x&& z > y )
+		System.out.println("z");
+		else
+		System.out.println("enter a number are not distrint");
+		
+	
+		
+		
+	}
+}
