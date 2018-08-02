662fa490f83c20bbd4018308769d8a8a3230425b
mounika444 <41731937+mounika444@users.noreply.github.com>
2018-08-02 02:12:00 -0700

    Create arthimetic1.java

---
+++ arthimetic1.java
@@ -0,0 +1,30 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class GFG
+{
+	static float SumOfAP(float a,float d,int n)
+	{
+
+		float sum=0;
+		for(int i= 0;i < n;i++)
+		{
+			
+			sum=sum+a;
+			a=a+d;
+		}
+		return sum;
+	}
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n=3;
+		float a=1,d=1;
+		//System.out.println(e);
+		int e=(int) SumOfAP(a,d,n);
+		System.out.println(e);
+	}
+}
