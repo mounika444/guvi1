19c541773275446f7ebd4b8061ca5bb055637e51
mounika444 <41731937+mounika444@users.noreply.github.com>
2018-08-11 00:29:03 -0700

    Rename strcat.java to strcat1.java

---
+++ strcat1.java
@@ -0,0 +1,20 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		 String s1,s2;	
+      Scanner sc=new Scanner(System.in);
+      s1=sc.next();
+      s2=sc.next();
+      System.out.println(s1+s2);
+     
+	
+	}
+}
