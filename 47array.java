a887c7c772afee45d6665cdd5e8b5e27816c6529
mounika444 <41731937+mounika444@users.noreply.github.com>
2018-08-12 21:23:14 -0700

    Create 47array.java

---
+++ 47array.java
@@ -0,0 +1,26 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int i;
+        int a[]=new int[n];
+        for(i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        Arrays.sort(a);
+        System.out.print(a[0]+" "+a[n-1]);
+    }
+
+}
+
+	
