ca0b85101606db691d8e37c8f6007d37af6c3c94
mounika444 <41731937+mounika444@users.noreply.github.com>
2018-08-16 09:22:19 -0700

    Update 79.java

--- 79.java
+++ 79.java
@@ -9,7 +9,6 @@ class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		// your code goes here
 		Scanner sc=new Scanner(System.in);
 		int n=sc.nextInt();
 		int m=sc.nextInt();
