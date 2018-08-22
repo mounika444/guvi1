d4012b3d312153ffb3b0eb33cf5804f20a33c3dd
mounika444 <41731937+mounika444@users.noreply.github.com>
2018-08-22 03:20:59 -0700

    Create 91a.java

---
+++ 91a.java
@@ -0,0 +1,28 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int length,height,width;
+	
+		int suf,vol;
+	
+		Scanner sc=new Scanner(System.in);
+		length=sc.nextInt();
+		height=sc.nextInt();
+		width=sc.nextInt();
+		suf=2*((length*height)+(height*width)+(width*length));
+		vol=length*height*width;
+
+		System.out.print(suf +" ");
+		System.out.print(vol);
+
+		
+	}
+}
