b4e2ee4952228e25946fc436ccc0d74849954932
mounika444 <41731937+mounika444@users.noreply.github.com>
2018-09-08 01:54:02 -0700

    Create player1.java

---
+++ player1.java
@@ -0,0 +1,16 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+StringBuffer sb=new StringBuffer("aabbcc");  
+	sb=sb.reverse() ; 
+	System.out.println(sb);
+		}
+}
